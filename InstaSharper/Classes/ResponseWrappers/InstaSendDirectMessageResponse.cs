f3a698efa50d2afbf106b8abfde52eec9bdc1a47
lordofhammsters <lordofhammsters@gmail.com>
2017-10-11 16:07:59 +0400

    Send direct message

---
+++ InstaSharper/Classes/ResponseWrappers/InstaSendDirectMessageResponse.cs
@@ -0,0 +1,10 @@
+﻿using InstaSharper.Classes.ResponseWrappers.BaseResponse;
+using System.Collections.Generic;
+
+namespace InstaSharper.Classes.ResponseWrappers
+{
+    internal class InstaSendDirectMessageResponse : BaseStatusResponse
+    {
+        public List<InstaDirectInboxThreadResponse> Threads { get; set; } = new List<InstaDirectInboxThreadResponse>();
+    }    
+}
