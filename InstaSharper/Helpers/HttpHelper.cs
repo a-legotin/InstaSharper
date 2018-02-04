5c58c11c33ed4cd0c0ece1ef1795faca92e04b59
Alexander Legotin <mrproz@yandex.ru>
2018-02-04 13:59:23 +0300

    Build fixed

--- InstaSharper/Helpers/HttpHelper.cs
+++ InstaSharper/Helpers/HttpHelper.cs
@@ -69,30 +69,4 @@ namespace InstaSharper.Helpers
             return request;
         }
     }
-
-    public static class HttpExtensions
-    {
-        public static Uri AddQuery(this Uri uri, string name, string value)
-        {
-            var httpValueCollection = HttpUtility.ParseQueryString(uri.Query);
-
-            httpValueCollection.Remove(name);
-            httpValueCollection.Add(name, value);
-
-            var ub = new UriBuilder(uri) {Query = httpValueCollection.ToString()};
-            return ub.Uri;
-        }
-
-        public static Uri AddQueryIfNotEmpty(this Uri uri, string name, string value)
-        {
-            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(value))
-                return uri;
-
-            var httpValueCollection = HttpUtility.ParseQueryString(uri.Query);
-            httpValueCollection.Remove(name);
-            httpValueCollection.Add(name, value);
-            var ub = new UriBuilder(uri) {Query = httpValueCollection.ToString()};
-            return ub.Uri;
-        }
-    }
 }
