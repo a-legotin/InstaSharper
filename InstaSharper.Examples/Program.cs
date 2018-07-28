38d39b86e48c2ed4d4be25cd33b56f47d475b3d5
Pedro Jesus <pedro.pj.souza@hotmail.com>
2018-07-28 16:01:29 -0300

    Alterações: readicionado program

--- InstaSharper.Examples/Program.cs
+++ InstaSharper.Examples/Program.cs
@@ -33,8 +33,8 @@ namespace InstaSharper.Examples
                 // create user session data and provide login details
                 var userSession = new UserSessionData
                 {
-                    UserName = "Jesopas",
-                    Password = "pedro123"
+                    UserName = "seila",
+                    Password = "seila"
                 };
 
                 var delay = RequestDelay.FromSeconds(2, 2);
