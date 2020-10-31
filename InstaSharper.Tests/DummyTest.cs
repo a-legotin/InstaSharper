51e4c80245a32aa9729a292c4c9a5fced32737c5
Alexander Legotin <mrproz@yandex.ru>
2020-10-31 15:04:25 +0300

    Added dummy test, update build scripts

---
+++ InstaSharper.Tests/DummyTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace InstaSharper.Tests
+{
+    [TestFixture]
+    [Category("AlwaysRun")]
+    public class DummyTest
+    {
+        [Test]
+        public void IsTrueReturned()
+        {
+            var isTrue = true;
+            Assert.IsTrue(isTrue, "should always return true");
+        }
+    }
+}
