3128bf27eaa51bfffa498d48fab0878e6f6d5b29
Alexander Legotin <mrproz@yandex.ru>
2020-11-29 19:40:47 +0300

    Add stream extensions

---
+++ InstaSharper/Utils/StreamExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace InstaSharper.Utils
+{
+    internal static class StreamExtensions
+    {
+        internal static byte[] ToByteArray(this Stream stream)
+        {
+            if (stream is MemoryStream memoryStream)
+                return memoryStream.ToArray();
+            using var ms = new MemoryStream();
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.CopyTo(ms);
+            return ms.ToArray();
+        }
+    }
+}
